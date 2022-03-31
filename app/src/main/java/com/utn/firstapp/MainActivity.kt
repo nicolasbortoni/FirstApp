7a8097ea61eec8bd4da9f2e4dfee7156a1851118
Nico <nicobortoni@frba.utn.edu.ar>
2022-03-31 19:58:39 -0300

    Correccion de warnings

--- app/src/main/java/com/utn/firstapp/MainActivity.kt
+++ app/src/main/java/com/utn/firstapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.utn.firstapp
 
-import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,24 +7,22 @@ import android.widget.TextView
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
-lateinit var txtCartel : TextView
-lateinit var btn1 : Button
-lateinit var btn2 : Button
-lateinit var btn3 : Button
-lateinit var btn4 : Button
-lateinit var Layout : ConstraintLayout
-
 class MainActivity : AppCompatActivity() {
-
-    val cartel1 = "TEXTO 1"
-    val cartel2 = "TEXTO 2"
-    var i1 = false
-    var i2 = false
-    var i3 = false
-    var i4 = false
-
+    lateinit var txtCartel : TextView
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btn4 : Button
+    lateinit var layout : ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val cartel1 = "TEXTO 1"
+        val cartel2 = "TEXTO 2"
+        var i1 = false
+        var i2 = false
+        var i3 = false
+        var i4 = false
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -34,46 +31,44 @@ class MainActivity : AppCompatActivity() {
         btn2 = findViewById(R.id.btn2)
         btn3 = findViewById(R.id.btn3)
         btn4 = findViewById(R.id.btn4)
-        Layout = findViewById(R.id.Layout)
+        layout = findViewById(R.id.Layout)
 
         btn1.setOnClickListener {
-            if(i1){
+            i1 = if(i1){
                 txtCartel.text = cartel1
-                i1 = false
+                false
             }
             else{
                 txtCartel.text = cartel2
-                i1 = true
+                true
             }
         }
         btn2.setOnClickListener {
-            if(i2){
+            i2 = if(i2){
                 txtCartel.setTextColor(Color.MAGENTA)
-                i2 = false
-            }
-            else {
+                false
+            } else {
                 txtCartel.setTextColor(Color.BLACK)
-                i2 = true
+                true
             }
         }
         btn3.setOnClickListener {
-            if(i3){
+            i3 = if(i3){
                 txtCartel.setBackgroundColor(Color.CYAN)
-                i3 = false
-            }
-            else {
+                false
+            } else {
                 txtCartel.setBackgroundColor(Color.WHITE)
-                i3 = true
+                true
             }
         }
         btn4.setOnClickListener {
-            if(i4){
-                Layout.setBackgroundColor(Color.YELLOW)
-                i4 = false
+            i4 = if(i4){
+                layout.setBackgroundColor(Color.YELLOW)
+                false
             }
             else {
-                Layout.setBackgroundColor(Color.WHITE)
-                i4 = true
+                layout.setBackgroundColor(Color.WHITE)
+                true
             }
         }
 
